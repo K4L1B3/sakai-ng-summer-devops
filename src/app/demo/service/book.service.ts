03e0f66d8f8784584cd423694cfe4d39d7c43988
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-12-13 15:34:30 -0300

    add try/catch and await/async in book service

--- src/app/demo/service/book.service.ts
+++ src/app/demo/service/book.service.ts
@@ -1,38 +1,67 @@
 import { Injectable } from '@angular/core';
-import { Book } from '../api/book';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Book } from '../api/book';
 
 @Injectable({
     providedIn: 'root',
 })
 export class BookService {
-    private basePath = "books"
+    private basePath = 'books';
 
     constructor(private db: AngularFireDatabase) { }
 
-    createBook(book: Book): any {
-        return this.db.list<Book>(this.basePath).push(book);
+    createBook(book: Book): Promise<any> {
+        return this.db.list<Book>(this.basePath)
+            .push(book)
+            .then((ref) => ref.key)
+            .catch((error) => {
+                console.error('Erro ao criar o livro:', error);
+                throw error;
+            });
     }
 
-    getBooks() {
-        return this.db.list<Book>(this.basePath).snapshotChanges().pipe(
-            map(changes =>
-                changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-            )
-        )
+    getBooks(): Observable<Book[]> {
+        return this.db.list<Book>(this.basePath)
+            .snapshotChanges()
+            .pipe(
+                map((changes) =>
+                    changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+                ),
+                catchError((error) => {
+                    console.error('Erro ao obter a lista de livros:', error);
+                    return throwError(error);
+                })
+            );
     }
 
-    getBookId(key: string): Observable<Book> {
-        return this.db.object<Book>(`${this.basePath}/${key}`).valueChanges();
+    getBookById(key: string): Observable<Book> {
+        return this.db.object<Book>(`${this.basePath}/${key}`)
+            .valueChanges()
+            .pipe(
+                catchError((error) => {
+                    console.error('Erro ao obter o livro por ID:', error);
+                    return throwError(error);
+                })
+            );
     }
 
-    updateBook(key: string, value: any): Promise<void> {
-        return this.db.object<Book>(`${this.basePath}/${key}`).update(value);
+    async updateBook(key: string, value: any): Promise<void> {
+        try {
+            await this.db.object<Book>(`${this.basePath}/${key}`).update(value);
+        } catch (error) {
+            console.error('Erro ao atualizar o livro:', error);
+            throw error;
+        }
     }
 
-    deleteBook(key: string): Promise<void> {
-        return this.db.object<Book>(`${this.basePath}/${key}`).remove();
+    async deleteBook(key: string): Promise<void> {
+        try {
+            await this.db.object<Book>(`${this.basePath}/${key}`).remove();
+        } catch (error) {
+            console.error('Erro ao excluir o livro:', error);
+            throw error;
+        }
     }
 }
